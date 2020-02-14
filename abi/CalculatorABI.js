5bde435b1bc29f06df76ab301dee666198ec006c
unknown <brid47@outlook.com>
2020-02-15 05:24:35 +0700

    before Hack

---
+++ abi/CalculatorABI.js
@@ -0,0 +1,119 @@
+  
+export var myCalculatorABI =
+
+
+[
+    {
+      "inputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "_from",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "res",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "diplay",
+      "type": "event"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_a",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_b",
+          "type": "uint256"
+        }
+      ],
+      "name": "myPlus",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "res",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_a",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_b",
+          "type": "uint256"
+        }
+      ],
+      "name": "myMinus",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "res",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "hello",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "text",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
--- store/web3.js
+++ store/web3.js
@@ -1,60 +1,62 @@
+import { myCalculatorABI } from '../abi//CalculatorABI'
 var Web3 = require('web3')
 // or
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 
 // dev provider
-web3.setProvider('ws://localhost:9545')
+//web3.setProvider('ws://localhost:9545')
 // console.log("is worked");
 // let listAccounts
 
+var myCalculatorAddress = '0xCd8887656D62eaa530c8e455e9452A63C8Dcb6Fa'
+let myCla = new web3.eth.Contract(myCalculatorABI, myCalculatorAddress)
 // web3.eth.getAccounts()
+function mytest() {
+  myCla.methods.myPlus(1,2).call().then(console.log).catch(console.error)
+
+}
+mytest();
+//console.log(test)
 export const state = () => ({
   ethAccountsList: {},
   accountsBalance: 0
 })
 
 export const mutations = {
-  setAccountsList (state, data) {
-        // console.log(' mutations called')
+  setAccountsList(state, data) {
+    // console.log(' mutations called')
     state.ethAccountsList = data
-        //
+    //
   },
-  setAccountsBalance (state, data) {
-        // console.log(' mutations called')
+  setAccountsBalance(state, data) {
+    // console.log(' mutations called')
     state.accountsBalance = data
-        //
+    //
   }
 }
 
 export const actions = {
-  async setAccountsList (context) {
+  async setAccountsList(context) {
     const data = await web3.eth.getAccounts().then(res => { // console.log(res);
       return res
     })
     await context.commit('setAccountsList', data)
   },
-  async setEthAccountsBalance (context,address) {
+  async setEthAccountsBalance(context, address) {
     console.log(address)
     const data = await web3.eth.getBalance(address).then(res => { // console.log(res);
       return res
     })
     await context.commit('setAccountsBalance', data)
   }
-//   setEthAccountsBalance: (context) => async (_id) => {
-//     const tokenBalance = await web3.eth.getBalance(_id).then(res => {
-//       return res
-//     })
-//     console.log(tokenBalance)
-//     await context.commit('setAccountsBalance', tokenBalance)
-//   }
 }
 export const getters = {
   getEthAccountsId: (state) => (_id) => {
     return state.ethAccountsList[_id]
-        //
+    //
   },
-  getEthAccountsBalance (state) {
+  getEthAccountsBalance(state) {
     return state.accountsBalance
-        //
+    //
   }
 }
